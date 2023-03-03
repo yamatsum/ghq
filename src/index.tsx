4d0b6e7d65bb7d13375fb0a723da90bfb208e8b5
yamatsum <sy12250@gmail.com>
2023-03-03 20:42:35 +0900

    fix: add ghq list command

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,49 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { ActionPanel, List, Action } from "@raycast/api";
+import { execSync } from "child_process";
 
 export default function Command() {
+  const stdout = execSync(`export PATH="$PATH:/opt/homebrew/bin:/usr/bin"; ghq list`);
+  const repositories: string[] = stdout
+    .toString()
+    .split(/\n/)
+    .filter((v) => v !== "");
+
+  const remoteRepositories: { [key: string]: string[] } = {};
+
+  repositories.map((repository) => {
+    const [remote, organization, name] = repository.split("/");
+
+    if (remoteRepositories[remote]?.length) {
+      remoteRepositories[remote].push(`${organization}/${name}`);
+    } else {
+      remoteRepositories[remote] = [`${organization}/${name}`];
+    }
+  });
+
   return (
     <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+      <>
+        {Object.keys(remoteRepositories).map((remote) => {
+          return (
+            <List.Section title={remote} key={remote}>
+              {remoteRepositories[remote].map((repository, index) => {
+                return (
+                  <List.Item
+                    key={index}
+                    icon="list-icon.png"
+                    title={repository}
+                    actions={
+                      <ActionPanel title="#1 in raycast/extensions">
+                        <Action.OpenInBrowser url={`https://${repository}`} />
+                      </ActionPanel>
+                    }
+                  />
+                );
+              })}
+            </List.Section>
+          );
+        })}
+      </>
     </List>
   );
 }
